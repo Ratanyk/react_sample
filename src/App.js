3b1957ecfc065455ff6eade99abbeeeefd072484
Ratanyk <myselfratan@gmail.com>
2020-10-24 10:10:11 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Routing from './components/routing.jsx';
+import './App.less';
+
+function App() {
+  return (
+    <div>
+        <Routing />
+      </div>
+  );
+}
+
+export default App;

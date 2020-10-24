adb9728e9982133dd1b043350ec2874fb3cc9cb3
Ratanyk <myselfratan@gmail.com>
2020-10-24 10:06:54 -0400

    Add files via upload

---
+++ webpack.dev.js
@@ -0,0 +1,62 @@
+const presetEnv = require.resolve("@babel/preset-env");
+const presetReact = require.resolve("@babel/preset-react");
+const classPropPlugin = require.resolve("@babel/plugin-proposal-class-properties");
+
+module.exports = {
+  mode: "development",
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              presetEnv, presetReact
+            ],
+            plugins: [classPropPlugin]
+          }
+        }
+      },
+      //LESS LOADER TO LOAD LESS FILES
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          }, {
+            loader: "css-loader" // translates CSS into CommonJS
+          }, {
+            loader: "less-loader" // compiles Less to CSS
+          }
+        ]
+      }, {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      //FONT-AWESOME FONT LOADER
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader"
+      }, {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader"
+      }, {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name(fileName) {
+                return `.${fileName}`;
+              },
+              emitFile: false
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
